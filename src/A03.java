de7dbc995007c15f4a4a9ef5674965a311a90f1a
Matilda_Chiang <chiang.matilda@gmail.com>
2019-03-11 19:04:44 -0700

    Attempting to add formulas

--- src/A03.java
+++ src/A03.java
@@ -11,6 +11,8 @@ public class A03 extends PApplet{
 	float x1,x2,x3,x4;
 	float y1,y2,y3,y4;
 	UIHelper u; 
+	float xP, yP;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,9 +24,9 @@ public class A03 extends PApplet{
     }
 
     public void setup(){
-    	g1 = new Gear(this, width/4, height/2);
-    	g2 = new Gear(this, 3*width/4, height/2-200);
-    	g3 = new Gear(this, 300, 200);
+    	g1 = new Gear(this, width/4, height/2,50);
+    	g2 = new Gear(this, 3*width/4, height/2-200,50);
+    	g3 = new Gear(this, 300, 200,100);
     }
 
     public void draw(){
@@ -37,23 +39,55 @@ public class A03 extends PApplet{
     	g2.drawMe();
     	g3.drawMe();
     	drawBelt(g1,g2);
-    	drawBelt(g1, g3);
+    	drawBelt(g3, g1);
     }
     
     public void drawBelt(Gear G1, Gear G2) {
-    	angle = atan((G1.yPos-G2.yPos)/(G1.xPos-G2.xPos));
-    	x1 = G1.xPos+50*sin(angle);
-    	x2 = G1.xPos-50*sin(angle);
-    	y1 = G1.yPos-50*cos(angle);
-    	y2 = G1.yPos+50*cos(angle);
     	
-    	x3 = G2.xPos+50*sin(angle);
-    	x4 = G2.xPos-50*sin(angle);
-    	y3 = G2.yPos-50*cos(angle);
-    	y4 = G2.yPos+50*cos(angle);
-    	
-    	line(x1,y1,x3,y3);
-    	line(x2,y2,x4,y4);
+    	//both radius is the same
+    	if(G1.radius==G2.radius) {    	
+	    	angle = atan((G1.yPos-G2.yPos)/(G1.xPos-G2.xPos));
+	    	x1 = G1.xPos+G1.radius*sin(angle);
+	    	x2 = G1.xPos-G1.radius*sin(angle);
+	    	y1 = G1.yPos-G1.radius*cos(angle);
+	    	y2 = G1.yPos+G1.radius*cos(angle);
+	    	
+	    	x3 = G2.xPos+G2.radius*sin(angle);
+	    	x4 = G2.xPos-G2.radius*sin(angle);
+	    	y3 = G2.yPos-G2.radius*cos(angle);
+	    	y4 = G2.yPos+G2.radius*cos(angle);
+	    	
+	    	line(x1,y1,x3,y3);
+	    	line(x2,y2,x4,y4);
+    	}
+    	if(G1.radius>G2.radius) { //g1 is ro and g2 is r1
+    		//find intersection point
+    		xP = (G2.xPos*G1.radius - G1.xPos*G2.radius)/(G1.radius-G2.radius);
+    		yP = (G2.yPos*G1.radius - G1.yPos*G2.radius)/(G1.radius-G2.radius);
+    		float xF1 = ((pow(G1.radius,2) * (xP-G1.xPos) + (G1.radius*(yP-G1.yPos)) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2))) + G1.xPos;
+        	float xF2 = ((pow(G1.radius,2) * (xP-G1.xPos) - (G1.radius*(yP-G1.yPos)) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2))) + G1.xPos;
+        	float yF1 = ((pow(G1.radius,2) * (xP-G1.yPos) - (G1.radius*(yP-G1.xPos)) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2))) + G1.yPos;
+        	float yF2 = ((pow(G1.radius,2) * (xP-G1.yPos) + (G1.radius*(yP-G1.xPos)) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2))) + G1.yPos;
+        	
+        	float xF3 = ((pow(G2.radius,2) * (xP-G2.xPos) + (G2.radius*(yP-G2.yPos)) * sqrt(pow((xP-G2.xPos),2) + pow((yP-G2.yPos),2)-pow(G2.radius,2)))/(pow(xP-G2.xPos,2) + pow(yP-G2.yPos,2))) + G2.xPos;
+        	float xF4 = ((pow(G2.radius,2) * (xP-G2.xPos) - (G2.radius*(yP-G2.yPos)) * sqrt(pow((xP-G2.xPos),2) + pow((yP-G2.yPos),2)-pow(G2.radius,2)))/(pow(xP-G2.xPos,2) + pow(yP-G2.yPos,2))) + G2.xPos;
+        	float yF3 = ((pow(G2.radius,2) * (xP-G2.yPos) - (G2.radius*(yP-G2.xPos)) * sqrt(pow((xP-G2.xPos),2) + pow((yP-G2.yPos),2)-pow(G2.radius,2)))/(pow(xP-G2.xPos,2) + pow(yP-G2.yPos,2))) + G2.yPos;
+        	float yF4 = ((pow(G2.radius,2) * (xP-G2.yPos) + (G2.radius*(yP-G2.xPos)) * sqrt(pow((xP-G2.xPos),2) + pow((yP-G2.yPos),2)-pow(G2.radius,2)))/(pow(xP-G2.xPos,2) + pow(yP-G2.yPos,2))) + G2.yPos;
+        	
+        	pushStyle();
+        	println(xF1);
+//        	strokeWeight(10);
+//        	point(xF1,yF2);
+//        	point(xF2, yF1);
+        	line(xF1,yF1,xF3,yF3);
+        	line(xF2,yF2,xF4,yF4);
+        	popStyle();
+    	}
+    }
+    
+    public void tangency(Gear G1, Gear G2) {
+    	float xF1 = (pow(G1.radius,2) * (xP-G1.xPos) + (yP-G1.yPos) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2));
+    	float xF2 = (pow(G1.radius,2) * (xP-G1.xPos) - (yP-G1.yPos) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2));
     }
     
     public void mousePressed() {
--- src/Gear.java
+++ src/Gear.java
@@ -6,18 +6,18 @@ public class Gear extends PApplet{
 	float xPos, yPos;
 	float radius; 
 	
-	Gear(PApplet parent, float x, float y){
+	Gear(PApplet parent, float x, float y, float r){
 		p = parent;
 		xPos = x;
 		yPos = y;
-	
+		radius = r;
 	}
 	
 	
 	void drawMe() {
 		p.pushMatrix();
 		p.translate(xPos, yPos);
-		p.ellipse(0, 0, 100, 100);
+		p.ellipse(0, 0, 2*radius, 2*radius);
 		p.popMatrix();
 	}
 	
