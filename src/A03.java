40036a318f2c6c5bef70eafbf3f5f9207e2233a7
Matilda_Chiang <chiang.matilda@gmail.com>
2019-03-12 19:26:32 -0700

    need to do recursion

--- src/A03.java
+++ src/A03.java
@@ -11,8 +11,10 @@ public class A03 extends PApplet{
 	float angle;
 	float x1,x2,x3,x4;
 	float y1,y2,y3,y4;
-	UIHelper u; 
+	ControlP5 cp5;
 	float xP, yP;
+	Slider rSlider;
+	Button addGear;
 	
 
 	public static void main(String[] args) {
@@ -25,27 +27,30 @@ public class A03 extends PApplet{
     }
 
     public void setup(){
-    	g1 = new Gear(this, width/4, height/2,50);
-    	g2 = new Gear(this, 3*width/4, height/2-200,200);
+    	cp5 = new ControlP5(this);
+    	g1 = new Gear(this, width/2, height/2,50);
+    	g2 = new Gear(this, 300+width/2, height/2-200,30);
     	g3 = new Gear(this, 300, 200,100);
     	g4 = new Gear(this, mouseX, 600, 30);
+    	initUI();
     }
 
     public void draw(){
     	background(255);
     	pushStyle();
     	stroke(2);
-    	rect(0,0,width/8, height);
+    	rect(0,0,200, height);
     	popStyle();
     	g1.drawMe();
-    	g2.drawMe();
+//    	g2.drawMe();
     	g3.drawMe();
-    	g4.drawMe();
-    	g4.xPos = mouseX;
-    	g4.yPos = mouseY;
-    	drawBelt(g1,g2);
+//    	g4.drawMe();
+//    	g4.xPos = mouseX;
+//    	g4.yPos = mouseY;
+//    	drawBelt(g1,g2);
     	drawBelt(g1, g3);
-    	drawBelt(g4,g1);
+//    	drawBelt(g4,g1);
+    	
     }
     
     public void drawBelt(Gear G1, Gear G2) {
@@ -90,30 +95,47 @@ public class A03 extends PApplet{
         	float xF4 = ((pow(G2.radius,2) * (xP-G2.xPos) - (G2.radius*(yP-G2.yPos)) * sqrt(pow((xP-G2.xPos),2) + pow((yP-G2.yPos),2)-pow(G2.radius,2)))/(pow(xP-G2.xPos,2) + pow(yP-G2.yPos,2))) + G2.xPos;
         	float yF3 = ((pow(G2.radius,2) * (yP-G2.yPos) - (G2.radius*(xP-G2.xPos)) * sqrt(pow((xP-G2.xPos),2) + pow((yP-G2.yPos),2)-pow(G2.radius,2)))/(pow(xP-G2.xPos,2) + pow(yP-G2.yPos,2))) + G2.yPos;
         	float yF4 = ((pow(G2.radius,2) * (yP-G2.yPos) + (G2.radius*(xP-G2.xPos)) * sqrt(pow((xP-G2.xPos),2) + pow((yP-G2.yPos),2)-pow(G2.radius,2)))/(pow(xP-G2.xPos,2) + pow(yP-G2.yPos,2))) + G2.yPos;
-        	
-        	
-        	
-        	
+        	        	
         	pushStyle();
         	strokeWeight(3);
         	point(xP,yP);
-        	//println(xF1);
-//        	
-//        	point(xF1,yF2);
-//        	point(xF2, yF1);
         	line(xF1,yF1,xF3,yF3);
-        	line(xF2,yF2,xF4,yF4);
-        	//line(xP,yP, xF1, yF1);
-        	
+        	line(xF2,yF2,xF4,yF4);        	
         	popStyle();
     	}
     }
     
-    public void tangency(Gear G1, Gear G2) {
-    	float xF1 = (pow(G1.radius,2) * (xP-G1.xPos) + (yP-G1.yPos) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2));
-    	float xF2 = (pow(G1.radius,2) * (xP-G1.xPos) - (yP-G1.yPos) * sqrt(pow((xP-G1.xPos),2) + pow((yP-G1.yPos),2)-pow(G1.radius,2)))/(pow(xP-G1.xPos,2) + pow(yP-G1.yPos,2));
+    void styleCaptionLabel(Label l) {
+    	  PFont font = createFont("arial", 16);
+    	  l.setLineHeight(20).setFont(font)
+    	    .setColor(0)
+    	    .align(ControlP5.LEFT_OUTSIDE, CENTER);
+    	  ;
+	}
+    
+    Slider createSlider(String key, String label, float x, float y, float min, float max) {
+    	  Slider s = cp5.addSlider(key).setPosition(x, y)
+    	    .setRange(min, max)
+    	    .setLabel(label)
+    	    .setSize(100, 20);
+    	  styleCaptionLabel(s.getCaptionLabel());
+    	  return s;
     }
     
+    Button createButton(String key, String label, float x, float y){
+    	  Button b = cp5.addButton(key)
+    	  .setLabel(label)
+    	  .setSize(75, 50)
+    	  .setPosition(x, y);
+//    	  styleButton(b);
+    	  return b;
+    }
+    
+    void initUI() {
+    	 rSlider = createSlider("radius", "Radius", 80, 50, 0, 255);
+    	 addGear = createButton("add","Add", 20,100);
+    }
+        
     public void mousePressed() {
     	println("hi");
     }
--- src/Gear.java
+++ src/Gear.java
@@ -5,19 +5,27 @@ public class Gear extends PApplet{
 	PApplet p;
 	float xPos, yPos;
 	float radius; 
+	Gear left, right;
 	
 	Gear(PApplet parent, float x, float y, float r){
 		p = parent;
 		xPos = x;
 		yPos = y;
 		radius = r;
+		left = null;
+		right = null;
 	}
 	
 	
 	void drawMe() {
 		p.pushMatrix();
-		p.translate(xPos, yPos);
+		p.translate(xPos, yPos);		
 		p.ellipse(0, 0, 2*radius, 2*radius);
+		p.line(0, 0, radius, 0);
+		p.line(0, 0, 0, radius);
+		p.line(0, 0, -radius, 0);
+		p.line(0, 0, 0, -radius);
+		p.ellipse(0,0,radius,radius);
 		p.popMatrix();
 	}
 	
